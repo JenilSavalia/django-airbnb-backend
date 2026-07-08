a00829dcf23179427ea8f89098f208191f2ff831
Jenil <jenil.savalia.cd@gmail.com>
2026-07-08 15:15:46 +0530

    readd

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -63,7 +63,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
     "https://django-airbnb-backend.vercel.app"
-    "https://django-airbnb-frontend-1oe9xiaai-jenil-savalias-projects.vercel.app"
+    "https://django-airbnb-frontend-1oe9xiaai-jenil-savalias-projects.vercel.app/"
 ]
 
 REST_AUTH = {
