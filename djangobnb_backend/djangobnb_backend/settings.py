9f394a919329fbf1346fb1f688ac62a38e951ff6
Jenil <jenil.savalia.cd@gmail.com>
2026-07-12 11:54:44 +0530

    settings typo fix

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -62,7 +62,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
-        https://django-airbnb-frontend-ten.vercel.app"
+    "https://django-airbnb-frontend-ten.vercel.app"
 ]
 
 REST_AUTH = {
