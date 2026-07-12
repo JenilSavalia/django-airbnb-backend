bae63aef3b11fa3ca60e82fe3b5ded26a7c387cd
Jenil <jenil.savalia.cd@gmail.com>
2026-07-12 13:28:41 +0530

    fix: add APP_DIRS option to TEMPLATES configuration

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -36,9 +36,19 @@ WEBSITE_URL = "https://django-airbnb-backend.vercel.app"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"]  # new
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,  # <-- YOU MUST ADD THIS LINE
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
     }
 ]
+
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
